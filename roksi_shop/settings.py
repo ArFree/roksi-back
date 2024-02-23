9a5bd92662f9602462f0f9ec3f6f671056d9a107
Olexander Bryl <edlrian814@gmail.com>
2024-02-23 17:27:40 +0200

    Fix bug with celery

--- roksi_shop/settings.py
+++ roksi_shop/settings.py
@@ -225,7 +225,6 @@ CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_BACKEND = "django-db"
 CELERY_TIMEZONE = "Europe/Kiev"
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 
